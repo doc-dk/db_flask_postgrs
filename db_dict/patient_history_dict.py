193ceb4819a15ff3780110986d7860780c7f0295
zaphod <dakelkar@gmail.com>
2018-11-30 12:40:24 +0530

    new mammo_usg forms; patient form TBD

--- db_dict/patient_history_dict.py
+++ db_dict/patient_history_dict.py
@@ -6,14 +6,25 @@ class PatientHistoryDict():
         self.value = value
 
     diet_dict = {'veg':"Vegetarian", 'non-veg':"Non-Vegetarian", 'egg':"Ovo-Vegetarian", 'other':"Other"}
-    diet_choice = CommonDict.generate_choice(diet_dict)
+    period_type_dict = {'regular':"Regular",'irregular':"Irregular",'other':"Other"}
+    menopausal_status_dict = {'pre':"Pre Menopausal",'post':"Post Menopausal",'peri':"Peri Menopausal",'other':"Other"}
     tobacco_dict =  {'no':'No exposure', 'passive':'Passive','active':'Active', 'pa':'Passive and Active'}
-    tobacco_choice = CommonDict.generate_choice(tobacco_dict)
     tobacco_type_passive_dict = {'no':'No exposure','home':"Home", 'work':"Work", 'commute':"Commute",
                                  'social':"Social Interactions", 'other':'Other'}
-    tobacco_type_passive_choice = CommonDict.generate_choice(tobacco_type_passive_dict)
     tobacco_type_dict = {'no':'No exposure','cig':"Cigarette", 'beedi':"Beedi", 'gutka':"Gutkha", 'pan_masala':"Pan Masala",
                          'jarda':"Jarda/Maava", 'hookah':"Hookah", 'patch':"Nicotine Patch", 'mishri':"Mishri",
                          'other':"Other"}
-    tobacco_type_choice = CommonDict.generate_choice(tobacco_type_dict)
     yes_no_dict = {'tbd': "To be filled", "N": "No", "Y": "Yes", 'other': "Other"}
+    current_complain_dict = {'self':"Self",'physician':"Physician",'screening_camp_id':"Screening Camp ID",'other':"Other"}
+    annual_income_dict = {'1':"0-2.5lacks",'2':"2.5-5lacks",'3':"5-10lacks",'4':"more than 10 lacks"}
+    metastasis_symptoms_dict = {'tbd': "To be filled", 'N': 'Absent', 'Rest of symptoms': 'TBD'}
+    #todo
+    diet_choice = CommonDict.generate_choice(diet_dict)
+    tobacco_choice = CommonDict.generate_choice(tobacco_dict)
+    tobacco_type_passive_choice = CommonDict.generate_choice(tobacco_type_passive_dict)
+    tobacco_type_choice = CommonDict.generate_choice(tobacco_type_dict)
+    current_complain_choice = CommonDict.generate_choice(current_complain_dict)
+    annual_income_choice = CommonDict.generate_choice(annual_income_dict)
+    period_type_choice = CommonDict.generate_choice(period_type_dict)
+    menopausal_status_choice = CommonDict.generate_choice(menopausal_status_dict)
+    metastasis_symptoms_choice = CommonDict.generate_choice(metastasis_symptoms_dict)
